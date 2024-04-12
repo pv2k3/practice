31f76d4d664c9562a59e04c1ade0f14ba9670dd9
pv2k3 <vermapriyanshu001@gmail.com>
2024-04-12 21:05:14 +0530

    20

--- html/Webtech/netflixNonInteractive/script.js
+++ html/Webtech/netflixNonInteractive/script.js
@@ -5,16 +5,23 @@ var disp = document.getElementsByClassName("card2");
 var c = 0;
 
 function actionDrop(index) {
-    if (c % 2 == 0) {
+    if (c%2==0) {
         button1[index].style.transform = "rotate(0)";
         disp[index].style.opacity = 1;
         disp[index].style.position = "relative";
-        c=c+1;
+        for (let i = 0; i < disp.length; i++) {
+            if (i != index) {
+                button1[i].style.transform = "rotate(45deg)";
+                disp[i].style.opacity = 0;
+                disp[i].style.position = "absolute";
+            }
+        }
+        c++;
     }
     else {
         button1[index].style.transform = "rotate(45deg)";
         disp[index].style.opacity = 0;
         disp[index].style.position = "absolute";
-        c=c+1;
+        c++;
     }
 }
