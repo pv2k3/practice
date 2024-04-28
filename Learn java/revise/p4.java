edb439981febd0897d22df03f7ab92c1ac89c4b6
pv2k3 <vermapriyanshu001@gmail.com>
2024-04-28 10:58:58 +0530

    update new

--- Learn java/revise/p4.java
+++ Learn java/revise/p4.java
@@ -1,4 +1,25 @@
 package revise;
 
+import java.util.Scanner;
+
 public class p4 {
+    static void merge_sort(int[] arr, int l, int r){
+        int len = arr.length;
+        if(arr.length == 2){
+            if(arr[l]>arr[r]){
+                int tmp = arr[l];
+                arr[l] = arr[r];
+                arr[r] = tmp;
+                return;
+            }
+        }
+        if(arr.length==1){
+            return;
+        }
+
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
 }
