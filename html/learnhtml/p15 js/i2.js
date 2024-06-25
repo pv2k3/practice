a924c9a2c184cd877e1e5a61671f7921e84288c7
pv2k3 <vermapriyanshu001@gmail.com>
2024-06-25 20:54:59 +0530

    reduce files 2

--- html/learnhtml/p15 js/i2.js
+++
@@ -1,9 +0,0 @@
-
-let a = [1,2,3,4,5];
-
-
-a.map((e,index,array)=>{
-    elementn
-})
-
-console.log(a);
--- html/learnhtml/p24/script.js
+++
@@ -1,4 +0,0 @@
-var slugify = require('slugify');
-
-let a = slugify("hello *& # !! everyone");
-console.log(a);
